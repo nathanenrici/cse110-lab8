8706fad2a777ff987cafaa59016ad6da53fcdb8d
nathanenrici <nenrici@ucsd.edu>
2022-11-19 17:23:21 -0800

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+names: Nathan Enrici
+
+1. I would run the automated tests locally before pushing the code. This would ensure that all tested problems could be fixed quickly, and the tests are written close to the code.
+2. no.
+3. Most likely not. Assuming there is some message() function, you would not be able to test its return value in a simple unit test as you would have to check on another device if it actually recieved the message properly.
+4. You could. If the message() function is able to return a reject/error statement, you could pass it messages that were too long and make sure it rejects them.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,67 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('8675309 is not a phone number', () => {
+    expect(functions.isPhoneNumber("8675309")).toBe(false);
+});
+test('925-299-8966 is a phone number', () => {
+    expect(functions.isPhoneNumber("925-299-8966")).toBe(true);
+});
+test('dn is not a phone number', () => {
+    expect(functions.isPhoneNumber("dn")).toBe(false);
+});
+test('111-111-1111 is a phone number', () => {
+    expect(functions.isPhoneNumber("111-111-1111")).toBe(true);
+});
+
+test('my email is an email', () => {
+    expect(functions.isEmail("nenrici@ucsd.edu")).toBe(true);
+});
+test('email must have an at', () => {
+    expect(functions.isEmail("nenriciucsd.edu")).toBe(false);
+});
+test('email must have a period', () => {
+    expect(functions.isEmail("nenrici@ucsdedu")).toBe(false);
+});
+test('short email works', () => {
+    expect(functions.isEmail("w@a.ss")).toBe(true);
+});
+
+test('a is not a strong password', () => {
+    expect(functions.isStrongPassword("a")).toBe(false);
+});
+test('aaaaaaaaaaaaaaaa is not a strong password', () => {
+    expect(functions.isStrongPassword("aaaaaaaaaaaaaaaa")).toBe(false);
+});
+test('a_12 is a strong password', () => {
+    expect(functions.isStrongPassword("a_12")).toBe(true);
+});
+test('abcd is a strong password', () => {
+    expect(functions.isStrongPassword("abcd")).toBe(true);
+});
+
+test('abcd is not a date', () => {
+    expect(functions.isDate("abcd")).toBe(false);
+});
+test('13/32/2002 is a date', () => {
+    expect(functions.isDate("13/32/2002")).toBe(true);
+});
+test('1/32/202 is not a date', () => {
+    expect(functions.isDate("1/32/202")).toBe(false);
+});
+test('1/1/2000 is a date', () => {
+    expect(functions.isDate("1/1/2000")).toBe(true);
+});
+
+test('isHexColor rejects based on length', () => {
+    expect(functions.isHexColor("x3")).toBe(false);
+});
+test('000 is a color', () => {
+    expect(functions.isHexColor("000")).toBe(true);
+});
+test('rejects based on wrong characters', () => {
+    expect(functions.isHexColor("x33000")).toBe(false);
+});
+test('000000 is a color', () => {
+    expect(functions.isHexColor("000000")).toBe(true);
+});
